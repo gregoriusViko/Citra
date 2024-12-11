6c2fb58d9147690bcfd6470184e1b179d73e0570
beetahdd <gregoriusangger93@gmai.com>
2024-12-12 00:25:06 +0700

    aktifin button masuk dan ambil gambar

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/home/gregorius-viko/My Task/My University/Pemrosesan Citra/build/assets/frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"D:\KULIAH SANATA DHARMA\SEMESTER 5\PEMROSESAN CITRA\UAS\Citra\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
